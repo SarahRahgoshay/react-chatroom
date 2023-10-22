b10a4d7955a3940994f10621e2d806f11ae8c289
sara-R98 <sarah.rahgoshay@gmail.com>
2023-10-22 18:35:59 +0330

    feat: add login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element= {<Login/>} />
+      </Routes>
     </div>
   );
 };
